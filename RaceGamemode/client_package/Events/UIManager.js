1a561e2d831c04efdb87d2a3df6136907db44a2c
Quentin Freire Novo <yushi76240@gmail.com>
2018-03-04 13:02:05 +0100

    WebUIManager update more lisibility

--- RaceGamemode/client_package/Events/UIManager.js
+++ RaceGamemode/client_package/Events/UIManager.js
@@ -1,5 +1,4 @@
-
-var deathUI = new WebUIWindow("race deathui", "package://race/ui/deathui.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var deathUI = new WebUIWindow("Race/DeathUI", "package://race/ui/deathui.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 deathUI.autoResize = true;
 deathUI.captureMouseInput = false;
 
@@ -10,24 +9,24 @@ deathUI.captureMouseInput = false;
  // Lobby
 
 
-var lobbylist = new WebUIWindow("ListOfLobby", "package://race/ui/lobby/MyamiLobby/LobbyList.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var lobbylist = new WebUIWindow("Race/ListOfLobby", "package://race/ui/lobby/MyamiLobby/LobbyList.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 lobbylist.autoResize = true;
 
-var lobbyLoading = new WebUIWindow("LoadindPage", "package://race/ui/lobby/MyamiLobby/loading.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var lobbyLoading = new WebUIWindow("Race/LoadingPage", "package://race/ui/lobby/MyamiLobby/loading.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 lobbyLoading.autoResize = true;
 
-var lobbyJoin = new WebUIWindow("LobbyJoin", "package://race/ui/lobby/MyamiLobby/LobbyJoin.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var lobbyJoin = new WebUIWindow("Race/LobbyJoin", "package://race/ui/lobby/MyamiLobby/LobbyJoin.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 lobbyJoin.autoResize = true;
 
-var lobbyPlayerServer = new WebUIWindow("LobbyJoin", "package://race/ui/lobby/MyamiLobby/PlayerServer.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var lobbyPlayerServer = new WebUIWindow("Race/LobbyJoin", "package://race/ui/lobby/MyamiLobby/PlayerServer.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 lobbyPlayerServer.autoResize = true;
 
 
-var Ingame = new WebUIWindow("Ingame", "package://race/ui/lobby/MyamiLobby/Ingame.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var Ingame = new WebUIWindow("Race/InGame", "package://race/ui/lobby/MyamiLobby/Ingame.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 Ingame.autoResize = true;
 
-var MapSelect = new WebUIWindow("MapSelect", "package://race/ui/lobby/MyamiLobby/MapSelect.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var MapSelect = new WebUIWindow("Race/MapSelect", "package://race/ui/lobby/MyamiLobby/MapSelect.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 MapSelect.autoResize = true;
 
-var TypeSelect = new WebUIWindow("TypeSelect", "package://race/ui/lobby/MyamiLobby/TypeSelect.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+var TypeSelect = new WebUIWindow("Race/TypeSelect", "package://race/ui/lobby/MyamiLobby/TypeSelect.html", new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 TypeSelect.autoResize = true;
